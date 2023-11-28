bf3fcdf1e863ddfd265012cac447dec44398079f
Łukasz Kowalewski <l.kowalewski.gaid@gmail.com>
2023-11-29 00:27:14 +0100

    Create tagger controller

---
+++ taggerController.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+const cataloguesFilePath = path.join('storage', 'catalogues.json');
+const metadataFilePath = path.join('storage', 'metadata.json');
+
+const readMetadata = () => {
+  if (fs.existsSync(metadataFilePath)) {
+    return JSON.parse(fs.readFileSync(metadataFilePath));
+  }
+  return [];
+};
+
+const saveMetadata = (metadata) => {
+  fs.writeFileSync(metadataFilePath, JSON.stringify(metadata, null, 2));
+};
+
+router.post('/:catalogueName/tag-images', (req, res) => {
+  const { catalogueName } = req.params;
+  const catalogues = JSON.parse(fs.readFileSync(cataloguesFilePath));
+  const metadata = readMetadata();
+
+  if (!catalogues[catalogueName]) {
+    return res.status(404).send('Catalogue not found');
+  }
+
+  catalogues[catalogueName].images.forEach((imageName) => {
+    const imageMetadata = metadata.find(
+      (meta) => meta.originalName === imageName
+    );
+    if (imageMetadata) {
+      const randomTag = Math.random() < 0.5 ? 'banana' : 'apple';
+      imageMetadata.tag = randomTag;
+    }
+  });
+
+  saveMetadata(metadata);
+  res.send(`Images in catalogue '${catalogueName}' tagged successfully.`);
+});
+
+module.exports = router;
