26a31e06b62a6d2a01d33518c5d9eed595de60f0
Łukasz Kowalewski <l.kowalewski.gaid@gmail.com>
2023-11-28 20:24:07 +0100

    Create catalogue controller

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const sharp = require('sharp'); 
+const sharp = require('sharp');
+const catalogueController = require('./catalogueController'); // Import the catalogue controller
+
 const port = 3000;
 
 const originalsDir = 'storage/originals';
@@ -12,23 +14,54 @@ fs.mkdirSync(originalsDir, { recursive: true });
 fs.mkdirSync(thumbnailsDir, { recursive: true });
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, originalsDir);
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(express.json());
+
+app.delete('/delete/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(originalsDir, filename);
+  const thumbnailPath = path.join(thumbnailsDir, `thumbnail-${filename}`);
+
+  if (fs.existsSync(filePath)) {
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error deleting the original file');
+      }
+
+      // Optionally delete the thumbnail
+      if (fs.existsSync(thumbnailPath)) {
+        fs.unlink(thumbnailPath, (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send('Error deleting the thumbnail file');
+          }
+        });
+      }
+
+      // Optionally update metadata file
+      // ... [Your code to update/delete the metadata from metadata.json]
+
+      res.send('File deleted successfully');
+    });
+  } else {
+    res.status(404).send('File not found');
   }
 });
 
-// Initialize Multer
-const upload = multer({ storage: storage });
-
-// Define a route for file upload
 app.post('/upload', upload.array('image', 10), async (req, res) => {
   try {
-    let metadataArray = []; // Initialize an array to collect metadata
+    let metadataArray = [];
 
-    // Read existing metadata from the file if it exists
     const metadataFilePath = path.join('storage', 'metadata.json');
     if (fs.existsSync(metadataFilePath)) {
       const existingMetadata = fs.readFileSync(metadataFilePath);
@@ -36,57 +69,59 @@ app.post('/upload', upload.array('image', 10), async (req, res) => {
     }
 
     if (req.files) {
-      await Promise.all(req.files.map(async file => {
-        const thumbnailFilename = `thumbnail-${file.originalname}`;
-        const thumbnailPath = path.join(thumbnailsDir, thumbnailFilename);
+      await Promise.all(
+        req.files.map(async (file) => {
+          const thumbnailFilename = `thumbnail-${file.originalname}`;
+          const thumbnailPath = path.join(thumbnailsDir, thumbnailFilename);
 
-        // Check if thumbnail already exists
-        if (!fs.existsSync(thumbnailPath)) {
-          await sharp(file.path)
-            .resize({
-              width: 200,
-              height: 200,
-              fit: sharp.fit.inside
-            })
-            .toFile(thumbnailPath);
-          
-          const metadata = {
-            originalName: file.originalname,
-            path: file.path,
-            thumbnailPath: thumbnailPath,
-            uploadDate: new Date().toISOString()
-          };
+          if (!fs.existsSync(thumbnailPath)) {
+            await sharp(file.path)
+              .resize({
+                width: 200,
+                height: 200,
+                fit: sharp.fit.inside,
+              })
+              .toFile(thumbnailPath);
 
-          metadataArray.push(metadata); // Add the new metadata to the array
-        }
-      }));
+            const metadata = {
+              originalName: file.originalname,
+              path: file.path,
+              thumbnailPath: thumbnailPath,
+              uploadDate: new Date().toISOString(),
+            };
 
-      // Write the updated array of metadata to the JSON file
+            metadataArray.push(metadata);
+          }
+        })
+      );
+
       fs.writeFileSync(
         metadataFilePath,
         JSON.stringify(metadataArray, null, 2)
       );
     }
 
-    res.send(`${req.files.length} files uploaded and thumbnails created successfully.`);
-  } catch(err) {
+    res.send(
+      `${req.files.length} files uploaded and thumbnails created successfully.`
+    );
+  } catch (err) {
     console.error(err);
     res.status(500).send('Error processing files');
   }
 });
-
-
 
 app.get('/download/:filename', (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(originalsDir, filename);
 
   if (fs.existsSync(filePath)) {
-    res.download(filePath); // Set the Content-Disposition header to attachment to prompt download
+    res.download(filePath);
   } else {
     res.status(404).send('File not found');
   }
 });
+
+app.use('/catalogue', catalogueController);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ catalogueController.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+const originalsDir = 'storage/originals';
+const cataloguesFilePath = path.join('storage', 'catalogues.json');
+
+const readCatalogues = () => {
+  if (fs.existsSync(cataloguesFilePath)) {
+    return JSON.parse(fs.readFileSync(cataloguesFilePath));
+  }
+  return {};
+};
+
+const saveCatalogues = (catalogues) => {
+  fs.writeFileSync(cataloguesFilePath, JSON.stringify(catalogues, null, 2));
+};
+
+router.post('/', (req, res) => {
+  const { name, images } = req.body;
+
+  if (!name) {
+    return res.status(400).send('Catalogue name is required');
+  }
+
+  const catalogues = readCatalogues();
+
+  if (catalogues[name]) {
+    return res.status(400).send('Catalogue already exists');
+  }
+
+  let existingImages = [];
+  if (Array.isArray(images)) {
+    existingImages = images.filter((image) =>
+      fs.existsSync(path.join(originalsDir, image))
+    );
+  }
+
+  catalogues[name] = {
+    creationDate: new Date().toISOString(),
+    images: existingImages,
+  };
+
+  saveCatalogues(catalogues);
+
+  res.send(`Catalogue '${name}' created successfully.`);
+});
+
+router.post('/:name/add-image', (req, res) => {
+  const { name } = req.params;
+  let { imageName } = req.body;
+
+  const catalogues = readCatalogues();
+
+  if (!catalogues[name]) {
+    return res.status(404).send('Catalogue not found');
+  }
+
+  if (!Array.isArray(imageName)) {
+    imageName = [imageName];
+  }
+
+  imageName = imageName.filter((img) => !catalogues[name].images.includes(img));
+
+  catalogues[name].images.push(...imageName);
+  saveCatalogues(catalogues);
+
+  res.send(`Images added to catalogue '${name}'.`);
+});
+
+router.post('/:name/remove-image', (req, res) => {
+  const { name } = req.params;
+  const { imageName } = req.body;
+
+  const catalogues = readCatalogues();
+
+  if (!catalogues[name]) {
+    return res.status(404).send('Catalogue not found');
+  }
+
+  const index = catalogues[name].images.indexOf(imageName);
+  if (index === -1) {
+    return res.status(400).send('Image not found in catalogue');
+  }
+
+  catalogues[name].images.splice(index, 1);
+  saveCatalogues(catalogues);
+
+  res.send(`Image '${imageName}' removed from catalogue '${name}'.`);
+});
+
+router.delete('/:name', (req, res) => {
+  const { name } = req.params;
+
+  const catalogues = readCatalogues();
+
+  if (!catalogues[name]) {
+    return res.status(404).send('Catalogue not found');
+  }
+
+  // Delete the catalogue
+  delete catalogues[name];
+  saveCatalogues(catalogues);
+
+  res.send(`Catalogue '${name}' removed successfully.`);
+});
+
+router.put('/:name/rename', (req, res) => {
+  const { name } = req.params;
+  const { newName } = req.body;
+
+  if (!newName) {
+    return res.status(400).send('New catalogue name is required');
+  }
+
+  const catalogues = readCatalogues();
+
+  if (!catalogues[name]) {
+    return res.status(404).send('Catalogue not found');
+  }
+
+  if (catalogues[newName]) {
+    return res.status(400).send('Catalogue with the new name already exists');
+  }
+
+  // Rename the catalogue
+  catalogues[newName] = { ...catalogues[name] };
+  delete catalogues[name];
+  saveCatalogues(catalogues);
+
+  res.send(`Catalogue '${name}' renamed to '${newName}' successfully.`);
+});
+
+module.exports = router;
